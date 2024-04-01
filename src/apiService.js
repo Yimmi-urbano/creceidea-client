f527b8e970861ccc38cb51a1a63ae8c3a04273bb
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-04-01 15:06:10 -0500

    url api

--- src/apiService.js
+++ src/apiService.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 async function fetchMenu() {
   try {
-    const response = await axios.get('https://creceidea.com/json/menu.json?v=5');
+    const response = await axios.get('https://recursing-banzai.74-208-244-91.plesk.page/json/menu.json?v=5');
     return response.data;
   } catch (error) {
     throw new Error('Error al obtener datos de la API');
@@ -12,7 +12,7 @@ async function fetchMenu() {
 
 async function fetchPages() {
   try {
-    const response = await axios.get('https://creceidea.com/json/pages.json?v=2');
+    const response = await axios.get('https://recursing-banzai.74-208-244-91.plesk.page/json/pages.json?v=2');
     return response.data;
   } catch (error) {
     throw new Error('Error al obtener datos de la API');
