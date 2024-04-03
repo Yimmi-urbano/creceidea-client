4e9207f5d9ea48b2222ea873774a97ab15820d31
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-04-02 20:08:08 -0500

    fix/cache

--- src/server.js
+++ src/server.js
@@ -1,53 +1,38 @@
-// src/server.js
 const express = require('express');
-const axios = require('axios');
 const path = require('path');
 const ejs = require('ejs');
-const {  fetchMenu } = require('./apiService');
-const { getPageByUrl,getPageByIdProduct } = require('./functions')
-
+const compression = require('compression');
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.set('views', path.join(__dirname, '..', 'views', 'templates', 'theme001'));
 app.set('view engine', 'ejs');
+
+app.use(compression());
+
+const cache = new Map();
+
+app.use((req, res, next) => {
+  const key = req.originalUrl || req.url;
+  const cachedContent = cache.get(key);
+  if (cachedContent) {
+    res.send(cachedContent);
+    return;
+  }
+  res.sendResponse = res.send;
+  res.send = (body) => {
+    cache.set(key, body);
+    res.sendResponse(body);
+  };
+  next();
+});
+
+
+app.use('/', routes);
+
 app.use(express.static(path.join(__dirname, '..', 'dist', 'public')));
-
-app.get('/', async (req, res) => {
-  try {
-    const pgeCont = await getPageByUrl('/');
-    res.render('index', { menuOptions: await fetchMenu(), pageTitle: pgeCont.title, contentHTML: pgeCont.content_html ,  contentTemplate: 'home' });
-  } catch (error) {
-    console.error('Error al obtener datos:', error);
-    res.status(500).send('Error al obtener datos');
-  }
-});
-
-app.get('/catalog/:rutaDinamica', async (req, res) => {
-  try {
-    const pgeCant = await getPageByIdProduct('/catalog/' + req.params.rutaDinamica);
-    console.log(pgeCant);
-    res.render('index', { menuOptions: await fetchMenu(), pageTitle: pgeCant.title, contentHTML: pgeCant.description_short ,  contentTemplate: 'product_detail'});
-  } catch (error) {
-    console.error('Error al manejar la solicitud:', error);
-    res.status(500).send('Error al manejar la solicitud');
-  }
-});
-
-app.get('/:rutaDinamica', async (req, res) => {
-  try {
-    const pgeCont = await getPageByUrl(req.params.rutaDinamica);
-    res.render('index', { menuOptions: await fetchMenu(), pageTitle: pgeCont.title, contentHTML: pgeCont.content_html ,contentTemplate: 'page'});
-  } catch (error) {
-    console.error('Error al manejar la solicitud:', error);
-    res.status(500).send('Error al manejar la solicitud');
-  }
-});
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
