a410ee6ab6c9448e3229a741c430d5def28c302f
Yimmi-urbano <139839538+Yimmi-urbano@users.noreply.github.com>
2024-04-28 14:38:21 -0500

    git upload news files

--- src/server.js
+++ src/server.js
@@ -3,27 +3,37 @@ const path = require('path');
 const ejs = require('ejs');
 const compression = require('compression');
 const routes = require('./routes');
+const validateSubdomain = require('./domainValidator');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Configuración del motor de plantillas EJS
 app.set('views', path.join(__dirname, '..', 'views', 'templates', 'theme001'));
 app.set('view engine', 'ejs');
 
-// Middleware de compresión de respuesta
-app.use(compression());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Habilitar la caché de vistas en Express
-app.set('view cache', true);
+app.use('/', async (req, res, next) => {
+    try {
+        const subdomain = req.hostname;
+        if (!subdomain) {
+            return res.status(403).send('No se proporcionó ningún subdominio');
+        }
+        const isValid = await validateSubdomain(subdomain);
 
-// Rutas
+        if (isValid) {
+            next();
+        } else {
+            res.status(403).send('Acceso no autorizado');
+        }
+    } catch (error) {
+        console.error('Error al validar el subdominio:', error);
+        res.status(500).send('Error al validar el subdominio');
+    }
+});
+
 app.use('/', routes);
 
-// Middleware para servir archivos estáticos
-app.use(express.static(path.join(__dirname, '..', 'dist', 'public')));
-
-// Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
